f2e7a4bed76f354ce0e939890e9abf0c466d18d8
gkimothi <gaurav.kimothi@gmail.com>
2022-01-27 18:37:30 -0800

    3rd

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from gsheetsdb import connect
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["GCP_SERVICE_ACCOUNT"],
+    st.secrets["gcp_service_account"],
     scopes=[
         "https://www.googleapis.com/auth/spreadsheets",
     ],
